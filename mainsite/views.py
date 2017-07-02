b18b071010a63221c59a9fa83423e268006d18d5
童话 <omg2hei@gmail.com>
2017-07-02 17:13:02 +0800

    增加功能、样式
    增加单页文章显示功能
    增加用户访问统计功能
    添加网站样式

--- mainsite/views.py
+++ mainsite/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Post
 from django.template.loader import get_template
 from datetime import datetime
+from django.shortcuts import redirect
 
 # Create your views here.
 def homepage(request):
@@ -10,11 +11,20 @@ def homepage(request):
     posts = Post.objects.all()
     now = datetime.now()
     html = templates.render(locals())
-    '''
-    post_list = list()
-    for count,post  in enumerate(posts):
-        post_list.append("No.{}".format(str(count)) + " " +str(post) + "<hr>")
-        post_list.append("<small>" + str(post.body.encode('utf-8'))\
-        + "</small><br><br>")
-    '''
     return HttpResponse(html)
+
+def navigation(request):
+    templates = get_template('navigation.html')
+    now = datetime.now()
+    html = templates.render(locals())
+    return HttpResponse(html)
+
+def showpost(request,slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from mainsite.views import homepage
+from mainsite.views import homepage,showpost,navigation
 
 urlpatterns = [
     url(r'^$',homepage),
+    url(r'^post/(\w+)$',showpost),
+    url(r'navigation/',navigation),
     url(r'^admin/', include(admin.site.urls)),
 ]
