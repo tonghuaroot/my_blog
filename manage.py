f2d3929b6361abf30ed3efe8d95544797bc18498
童话 <omg2hei@gmail.com>
2018-07-31 18:02:12 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-# -----BEGIN RSA PRIVATE KEY-----
+# del rsa key
 # test
